8bde1880763991c9e146f827739814d1c636b2e1
weetsapp <69223442+weetsapp@users.noreply.github.com>
2020-08-14 00:28:35 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include "header/runtime.h"
+#include "header/Uinput.h"
+#include "header/file.h"
+using namespace std;
+/*phayro is git clone compiler 
+email: geckosearch2018@gmail.com
+*/
+void t(){
+	logInputUser:
+	
+Uinput getUserTarget;
+std::string pfile = getUserTarget.bpackName;
+
+struct packageName{
+std::string repo;	
+	
+};
+
+flp:
+
+std::cout <<"Enter a package Name: ";
+std::cin >> pfile;
+packageName pack;
+pack.repo;
+pack.repo=pfile;
+std::cout <<pack.repo;
+
+
+std::cout <<"Creating a restore point"<<endl;
+runF();
+
+
+for(int ra = 1; ra <100; ra++){
+std::cout <<"Writing file..................\t"<<endl;
+
+}
+std::cout <<"Success" <<endl;
+std::string urL = "git clone https://github.com/";
+std::string url1 = urL+pack.repo;
+
+std::cout <<"Connecting to " << url1 <<endl;
+
+system("git clone https://github.com/phayro");
+
+
+std::cout <<"Would you like to try again?(y/n): ";
+std::string vput;
+std::cin>>vput;
+
+if(vput=="y"){
+goto flp;	
+}
+else{
+std::cout <<"Exiting Phayro               \t[ok]"<<endl;	
+}
+
+};
+
+int main()
+{
+	
+	
+	
+	testPublicRunTime tim;
+	tim.runtime = "3000";
+std::cout <<"detecting..." <<endl;
+std::string runhr = tim.runtime;
+
+	if(runhr>="3000"){
+std::cout <<"booting runtime           \t [ok]"<<endl;
+std::cout <<"==================="<<endl;
+std::cout <<"=====WELCOME PHAYRO======="<<endl;
+std::cout <<"==================="<<endl;
+
+std::cout <<"NOTE: this tool will install repositories from github.com"<<endl;
+
+
+t();
+
+
+
+
+	}
+	else{
+		std::cout <<" fail to excute runtime enviroment";
+
+}
+
+
+
+
+
+}
