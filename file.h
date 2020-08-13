b8dd43ec7ed599c7a84bebd7286075959f169dde
weetsapp <69223442+weetsapp@users.noreply.github.com>
2020-08-14 00:34:03 +0100

    Add files via upload

---
+++ file.h
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+//this is a C language code
+//modify this if you want
+
+/***
+this is based on open source see GPL for more documentation
+***/
+void runF(){
+FILE *sd;
+
+sd = fopen("restore.conf", "w");
+fprintf(sd, "");
+printf("Success...");
+
+
+fclose(sd);
+};
+
+
